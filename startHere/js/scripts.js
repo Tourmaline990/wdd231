f8f540907f64d88f41bee0debacbf0add3b61f32
Aaliya <aagbetoba@byupathway.edu>
2026-01-30 12:25:57 +0100

    commit

---
+++ startHere/js/scripts.js
@@ -0,0 +1,12 @@
+const getString = window.location.search;
+console.log(getString);
+const myInfo = new URLSearchParams(getString);
+console.log(myInfo)
+
+console.log(myInfo.get("first"));
+document.querySelector("#results").innerHTML = `
+<p>Appointment for ${myInfo.get("first")} ${myInfo.get("last")}</p>
+<p>Proxy ${myInfo.get("ordinance")} ${myInfo.get("date")} in the ${myInfo.get("location")} temple </p>
+<p>Your Phone: ${myInfo.get("phone")} </p>
+<p>Your Email is: ${myInfo.get("email")} </p>
+`
